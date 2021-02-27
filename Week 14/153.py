27e5d44e02633bea466219a3b7f85948772bbcb4
Hoang Cao Bao <62338248+hoangcaobao@users.noreply.github.com>
2021-02-27 10:14:37 +0700

    Delete Week 14 directory

--- Week 14/153.py
+++
@@ -1,6 +0,0 @@
-### DO NOT REMOVE THIS
-from typing import List
-### DO NOT REMOVE THIS
-class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        return min(nums)
--- Week 14/50.py
+++
@@ -1,6 +0,0 @@
-### DO NOT REMOVE THIS
-from typing import List
-### DO NOT REMOVE THIS
-class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        return x**n
