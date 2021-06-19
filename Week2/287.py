8d35559b8b3ca1791007dc7952538d8b42024306
Hoang Cao Bao <62338248+hoangcaobao@users.noreply.github.com>
2021-06-19 16:06:04 +0700

    Create 287.py

---
+++ Week2/287.py
@@ -0,0 +1,8 @@
+import collections
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+    
+        nums.sort()
+        for i in range(0,len(nums)):
+            if i+1<len(nums) and nums[i]==nums[i+1]:
+                return nums[i]
