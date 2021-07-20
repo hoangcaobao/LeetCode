ba52e8a4118b0cd7d185e79f0f3074f19c4a434a
Hoang Cao Bao <62338248+hoangcaobao@users.noreply.github.com>
2021-07-20 08:41:20 +0700

    Update 1324.py

--- Week1/1324.py
+++ Week1/1324.py
@@ -1,12 +1,25 @@
 class Solution:
     def printVertically(self, s: str) -> List[str]:
-        result=""
-        mang=[]
-        for i in range(len(s)):
-            if s[i]!=" ":
-                result+=s[i]
-            else:
-                mang.append(result)
-                result=""
-        mang.append(result)
-        return mang
+        x=s.split(" ")
+        dodailonnhat=0
+        result=[]
+        chu=""
+        
+        for i in range(0,len(x)):
+            if len(x[i])>dodailonnhat:
+                dodailonnhat=len(x[i])
+        for i in range(0,len(x)):
+            x[i]+=((dodailonnhat-len(x[i]))*" ")
+            
+        for i in range(0,len(x)):
+            if len(x[i])>dodailonnhat:
+                dodailonnhat=len(x[i])
+        for i in range(0,dodailonnhat):
+            for j in range(0,len(x)):
+                chu+=x[j][i]
+            result.append(chu)
+            chu=""
+        for i in range(0,len(result)):
+            result[i]=result[i].rstrip()
+                
+        return result
